772cc864d6ede81f29efbf8a41ba0834f0222f97
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-10-17 14:10:07 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan31-Perkenalan Mahasiswa/src/if01/pkg10118016/latihan31/perkenalan/mahasiswa/IF0110118016Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan31.perkenalan.mahasiswa;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Perkenalan Mahasiswa
+ */
+public class IF0110118016Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mhs = new Mahasiswa();
+           
+            
+            //output
+            mhs. nim = "10118016";
+            mhs.nama ="Rezky Nur Fauzi";
+            mhs.perkenalanDiri();
+            
+            mhs.nim = "10118013";
+            mhs.nama = "Rifian Joe";
+            mhs.perkenalanDiri();
+    }
+    
+}
---
+++ IF01-10118016-Latihan31-Perkenalan Mahasiswa/src/if01/pkg10118016/latihan31/perkenalan/mahasiswa/Mahasiswa.java
@@ -0,0 +1,20 @@
+package if01.pkg10118016.latihan31.perkenalan.mahasiswa;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Mahasiswa {
+
+	public String nim;
+	public String nama;
+
+	public void perkenalanDiri() {
+	      
+                          System.out.println("Hallo Everyone");
+                          System.out.println("My NIM is " + nim);
+                          System.out.println("My name is " + nama);
+                          System.out.println();
+	}
+
+}
